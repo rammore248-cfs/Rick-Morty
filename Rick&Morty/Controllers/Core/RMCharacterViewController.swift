c38ff7cbc77c52fbe61b82b813ec39d07c5ab9fe
Ramchandra Mahadev More <rammore248@gmail.com>
2024-11-26 12:01:17 +0530

    Character List View

--- Rick&Morty/Controllers/Core/RMCharacterViewController.swift
+++ Rick&Morty/Controllers/Core/RMCharacterViewController.swift
@@ -10,19 +10,18 @@ import UIKit
 // controller to show and search for Characters
 class RMCharacterViewController: UIViewController {
 
+    private let characterListView = CharacterListView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Characters"
-
-        RMService.shared.execute(.listCharactersRequests, expecting: RMGetAllCharacterResponse.self) { result in
-            switch result {
-            case .success(let model):
-                print("Total: "+String(model.info.pages))
-                print("Page Result count: "+String(model.results.count))
-            case .failure(let error):
-                print(String(describing: error))
-            }
-        }
+        view.addSubview(characterListView)
+        NSLayoutConstraint.activate([
+            characterListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            characterListView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            characterListView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            characterListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
